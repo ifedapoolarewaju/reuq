a523981d6e34207ab18ba95a5f5af659dd6da854
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2016-12-24 20:42:56 +0100

    refactor: remove vimeo api

--- examples/store/js/app.js
+++ examples/store/js/app.js
@@ -1,34 +1,16 @@
 var controller = {
-  callbacks: {
-    renderSearch: function(data, status, form) {
-      form[0].reset();
-      this.render('searchedVideos', data.data);
-    }
-  },
-  eventHandlers: {
-    close: function(el) {
-      el.parents('.media_list').remove();
-    }
-  },
-  fn: {
-    videoId: function() {
-      return this.uri.substr(8);
+  dynamicProperties: {
+    videoId: function(data) {
+      return data.uri.substr(8);
     },
-    nameTrunc: function() {
-      return this.name.substr(0, 25) + "...";
-    },
-    searchFormHeaders: function() {
-      return this.controller.resources.videos.headers;
+    nameTrunc: function(data) {
+      return data.name.substr(0, 25) + "...";
     }
   },
   resources: {
     videos: {
-      url:"https://api.vimeo.com/tags/fun/videos?per_page=10",
-      dataKey: "data",
-      headers: {
-        Authorization: 'bearer 34210aeac4e02a251b8821a53620e93c',
-        Accept: 'application/vnd.vimeo.*+json;version=3.0'
-      }
+      url:"resources/videos.json",
+      dataKey: "data"
     }
   }
 }
